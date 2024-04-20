f2df4805f273772887b5faaa5c507d85f229dc28
Thomas Galliker <thomas@bluewin.ch>
2024-04-20 13:52:23 +0200

    Add unit test to demonstrate the use of MauiMocks.Init and MauiMocks.Reset

---
+++ Tests/MauiMocks.Tests/EventToCommandBehaviorTests.cs
@@ -0,0 +1,51 @@
+using CommunityToolkit.Maui.Behaviors;
+using FluentAssertions;
+using Xunit;
+
+namespace Microsoft.Maui.Tests
+{
+    public class EventToCommandBehaviorTests : IDisposable
+    {
+        public EventToCommandBehaviorTests()
+        {
+            MauiMocks.Init();
+        }
+
+        [Fact]
+        public void ShouldRaiseItemSelectedEvent_IfSelectedItemIsSet()
+        {
+            // Arrange
+            var didEventToCommandBehaviorFire = false;
+
+            var listView = new ListView
+            {
+                BindingContext = new object(),
+                ItemsSource = new[]
+                {
+                    "Item 1",
+                    "Item 2",
+                    "Item 3",
+                },
+                Behaviors =
+                {
+                    new EventToCommandBehavior
+                    {
+                        EventName = nameof(ListView.ItemSelected),
+                        Command = new Command(() => didEventToCommandBehaviorFire = true)
+                    }
+                }
+            };
+
+            // Act
+            listView.SelectedItem = "Item 2";
+
+            // Assert
+            didEventToCommandBehaviorFire.Should().BeTrue();
+        }
+
+        public void Dispose()
+        {
+            MauiMocks.Reset();
+        }
+    }
+}
